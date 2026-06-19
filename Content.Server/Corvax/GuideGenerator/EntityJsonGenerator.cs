840601e49ecaaf09c45babe8ac8e5c07a53c88dd
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-06-19 17:40:32 +0300

    fix

--- Content.Server/Corvax/GuideGenerator/EntityJsonGenerator.cs
+++ Content.Server/Corvax/GuideGenerator/EntityJsonGenerator.cs
@@ -16,6 +16,9 @@ public sealed class EntityJsonGenerator
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
     };
 
+    [JsonIgnore]
+    public string Id { get; }
+
     [JsonPropertyName("name")]
     public string Name { get; }
 
@@ -35,6 +38,7 @@ public sealed class EntityJsonGenerator
         var prototypeManager = IoCManager.Resolve<IPrototypeManager>();
         var loc = IoCManager.Resolve<ILocalizationManager>();
 
+        Id = proto.ID;
         Name = TextTools.CapitalizeString(TextTools.GetDisplayName(proto, prototypeManager, loc));
         Description = proto.Description;
 
